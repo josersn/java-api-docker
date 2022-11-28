2d9c7a5a5ba2fd2a1172d2a6399c070765b9211a
josersn <nettorammos@hotmail.com>
2022-11-27 21:50:54 -0300

    infra layer

--- src/main/java/com/docker/java/controllers/TaskController.java
+++ src/main/java/com/docker/java/controllers/TaskController.java
@@ -1,7 +1,42 @@
 package com.docker.java.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.docker.java.DTOs.TaskDTO;
+import com.docker.java.services.TasksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TaskController {
+
+    @Autowired
+    private TasksService service;
+
+    @PostMapping
+    @ResponseBody
+    public TaskDTO create(@RequestBody TaskDTO taskDTO) {
+        return service.create(taskDTO);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public TaskDTO update(@PathVariable("id") Long id,
+                               @RequestBody TaskDTO task) {
+        return service.update(task);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<TaskDTO> list() {
+        return service.get();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public String delete(@PathVariable("id") Long id) {
+        return service.delete(id);
+    }
 }
